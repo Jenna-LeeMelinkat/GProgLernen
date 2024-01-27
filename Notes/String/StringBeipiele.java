2208c1fc10aac7436c10128e1e478b5c3ceec618
Jenna-LeeMelinkat <lindenstr31@gmx.de>
2024-01-27 18:44:19 +0100

    StringBeispiele

---
+++ Notes/String/StringBeipiele.java
@@ -0,0 +1,40 @@
+package Notes.String;
+
+public class StringBeipiele {
+
+    //length()
+    String str = "Hallo Welt";
+    int length = str.length(); // length ist 10
+
+    //-------------------------------------------------------------
+
+    //startsWith()
+    boolean startsWithHallo = str.startsWith("Hallo"); // true
+
+    //-------------------------------------------------------------
+
+    //endsWith()
+    boolean endsWithWelt = str.endsWith("Welt"); // true
+
+    //-------------------------------------------------------------
+
+    //split()
+    String[] parts = str.split(","); // parts ist ein Array: {"Hallo", "Welt", "Test"}
+
+    //-------------------------------------------------------------
+
+    //contains()
+    boolean containsWelt = str.contains("Welt"); // true
+
+    //-------------------------------------------------------------
+
+    //toUpperCase()
+    String upperCaseStr = str.toUpperCase(); // upperCaseStr ist "HALLO WELT"
+
+    //-------------------------------------------------------------
+
+    //substring()
+    String subStr = str.substring(6); // subStr ist "World"
+    String subStr2 = str.substring(0, 5); // subStr2 ist "Hello"
+
+}
